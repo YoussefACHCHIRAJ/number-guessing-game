053b6594a04f42c717297aaf627a891626279a1b
youssef ACHCHIRAJ <youssefachchirraje66@gmail.com>
2022-09-22 10:13:23 +0100

    changes

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let enterButton = document.querySelector(".enter");
 let messageBox = document.querySelector(".alert");
 let message = document.querySelector(".alert span");
 let playAgain = document.querySelector(".repeat");
+let about = document.querySelector(".about");
 let numberToGuesse =0;
 startButton.addEventListener("click",()=>{
     document.querySelector(".intro").style.display = "none";
     document.querySelector("section").style.display = "block";
 })
+chooseNumber();
 enterButton.addEventListener("click",()=>{
     if(input.value == "" || +input.value >10){
         alert("enter number between 0 and 9");    
@@ -17,29 +19,32 @@ enterButton.addEventListener("click",()=>{
         text.innerHTML = input.value;
         document.querySelector(".result").appendChild(text);
         let content = document.querySelectorAll("p").length;
+        
     if(input.value == numberToGuesse){
             enterButton.style.display = "none";
             text.style.backgroundColor = "green"; 
             messageBox.style.display = "block";
             messageBox.style.backgroundColor = "green";
             message.innerHTML = `Congratulation , you find the number`;
-            // playAgain.style.display = "block";
         }else if(content == 3){
             enterButton.style.display = "none";
             messageBox.style.display = "block";
             messageBox.style.backgroundColor = "red";
             message.innerHTML = `Sorry, you lose the number was ${numberToGuesse}`;
-            // playAgain.style.display = "block";
             }
         }
     input.value = "";
 })
 
 function chooseNumber(){
-    let numbers = "123456789";
+    let numbers = "0917634528";
     let myNumber =0;
     let index= Math.floor(Math.random()*numbers.length);
     myNumber = numbers[index];
     numberToGuesse = myNumber;
+    if(numberToGuesse % 2 ==0){
+        about.innerHTML  = "the number is pair";
+    }else{
+        about.innerHTML  = "the number is impair";
+    }
 }
-chooseNumber();
